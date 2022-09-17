69e21a4f371ecab752593106aeea3eb92da25bf3
Ricardo <24703825+RickyM7@users.noreply.github.com>
2022-09-17 00:57:09 -0300

    Aquivos adicionados via upload
    Problemas do Beecrowd:
    - 1117
    - 1132
    - 1143

---
+++ nota_valida.py
@@ -0,0 +1,13 @@
+x, y = (-1.0,-1.0)
+
+while (x < 0.0 or x > 10.0) or (y < 0.0 or y > 10.0):
+    if x < 0.0 or x > 10.0:
+        x = float(input())
+        if x < 0.0 or x > 10.0:
+            print('nota invalida')
+    if y < 0.0 or y > 10.0:
+        y = float(input())
+        if y < 0.0 or y > 10.0:
+            print('nota invalida')
+
+print(f'media = {(x+y)/2:.2f}')
